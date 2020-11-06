caeadb94b32d35fb329e23651ce0943b65dfc646
Nate Eizenga <eizengan@gmail.com>
2020-11-05 20:33:47 -0500

    Replace reference to WIP name

--- lib/reforge/zeitwerk.rb
+++ lib/reforge/zeitwerk.rb
@@ -2,7 +2,7 @@
 
 require "zeitwerk"
 
-# TRICKY: Zeitwerk::Loader.for_gem would work if this were in lib/constitute.rb, but we have initialization here to
+# TRICKY: Zeitwerk::Loader.for_gem would work if this were in lib/reforge.rb, but we have initialization here to
 # keep loader logic consolidated. We need to reconstruct the work the work done by that method, and to do so correctly
 # from any location we need to establish a path to our root module and its home directory
 reforge_path = "#{__dir__}/../reforge.rb"
