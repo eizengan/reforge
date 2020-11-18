ae73ffff50d09a8eef4eb12b5779b8f3a9e0aee9
Nate Eizenga <eizengan@gmail.com>
2020-11-17 23:11:19 -0500

    MemoizedTransform by: option uses a Transform

--- spec/reforge/transform/memoized_transform_spec.rb
+++ spec/reforge/transform/memoized_transform_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe Reforge::Transform::MemoizedTransform do
     let(:memoize_configuration) { { by: 10 } }
 
     it "raises an ArgumentError during initialization" do
-      expect { instance }.to raise_error ArgumentError, "The :by option of the configuration hash must be callable"
+      expect { instance }.to raise_error ArgumentError, "The :by option of the configuration hash must be callable or a transform configuration hash"
     end
   end
 end
